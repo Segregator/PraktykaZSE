db160f0bfe1128caeb6c20a6f0ffe548cf98f7ed
Segregator <79479992+Segregator@users.noreply.github.com>
2021-02-23 14:52:36 +0100

    Projekt

---
+++ Zadania/src/zadanie3.java
@@ -0,0 +1,10 @@
+public class zadanie3 {
+    public static void main(String[] args) {
+
+        double a = 7;
+        double sqrt = Math.sqrt(9);
+        double pole = ((a * a) * sqrt)/4;
+        System.out.println("pole = " + pole );
+
+    }
+}
---
+++ Zadania/src/zadanie4.java
@@ -0,0 +1,9 @@
+public class zadanie4 {
+    public static void main(String[] args) {
+
+        String imie = "Adrian";
+        String nazwisko = " Jackowiak";
+        System.out.println(" Imie i nazwisko :" + imie + nazwisko);
+    }
+
+}
